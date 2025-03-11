563b75a292c825a369e53f70cc371e90116e6668
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-03-11 13:51:57 +0000

    Add conf.py

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'wish-list'
+copyright = '2025, Ayaz Salikhov'
+author = 'Ayaz Salikhov'
+
+version = 'latest'
+release = 'latest'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# The file above was generated using sphinx 8.2.3 with this command:
+# sphinx-quickstart --project "wish-list" --author "Ayaz Salikhov" -v "latest" -r "latest" -l en --no-sep --no-makefile --no-batchfile
+# These are custom options for this project
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+.. wish-list documentation master file, created by
+   sphinx-quickstart on Tue Mar 11 13:50:55 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+wish-list documentation
+=======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
